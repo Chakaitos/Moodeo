891fb4fa349635105eb746ab0dd903bd7d79db02
Drew Garcia <drewgarcia23@gmail.com>
2014-04-07 11:56:37 -0500

    Yo

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require_relative './lib/moodeo.rb'
 require 'sinatra'
 require 'rubygems'
+require 'pry-debugger'
+
+Moodeo.db_name = 'moodeo.db'
 
 set :bind, '0.0.0.0'
 
@@ -15,21 +18,41 @@ end
 post '/signin' do
   result = Moodeo::SignIn.run(:username => params[:username], :password => params[:password])
   if result.success?
+    @usernamedisplay = params[:username]
     erb :home
-  elsif result.error == :username_not_found
-    @message = "Sorry, username is not found"
-    erb :signin
-  elsif result.error == :password_not_correct
-    @message = "Sorry, password is not correct. Try again"
-    erb :signin
+  else
+    if result.error == :invalid_username
+      @message = "Sorry, username is not found"
+    end
+    if result.error == :invalid_password
+      @message = "Sorry, password is not correct. Try again"
+    end
+      puts @message
+      erb :signin
   end
 end
 
 get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
   @name = params[:name]
   @username = params[:username]
   @password = params[:password]
-  erb :signup
+  result = Moodeo::SignUp.run(:name => @name, :username => @username, :password => @password)
+  if result.success?
+    erb :signin
+  else
+    if result.error == :username_taken
+      @message = "Sorry, that username is taken"
+    end
+    if result.error == :weak_password
+      @message = "Sorry, that password is weak sauce."
+    end
+      puts @message
+      erb :signup
+  end
 end
 
 # get '/home' do
--- lib/moodeo.rb
+++ lib/moodeo.rb
@@ -1,6 +1,7 @@
 module Moodeo
 end
 
+require 'sqlite3'
 require_relative 'moodeo/user.rb'
 require_relative 'moodeo/database.rb'
 require_relative 'moodeo/session.rb'
