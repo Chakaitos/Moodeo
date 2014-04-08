239cc6e1b89ab25170f42cc653e1324d895bc9ca
Jose R. Borja <chakaitos@gmail.com>
2014-04-07 22:59:55 -0500

    Refactor

--- lib/moodeo/user.rb
+++ lib/moodeo/user.rb
@@ -2,12 +2,11 @@ class User
  	attr_accessor :name, :password, :username, :id
  	@@counter = 0
 
-  def initialize(name,username,password,token=nil)
+  def initialize(name,username,password)
     @name = name
     @username = username
     @password = password
     @@counter += 1
     @id = @@counter
-    @token_id = token
   end
 end
--- spec/video_spec.rb
+++ spec/video_spec.rb
@@ -1,11 +1,11 @@
 require './spec/spec_helper.rb'
 
 describe 'Video' do
- it "initializes a video with a name, genre, and url" do
-
-   video = Video.new("Drew eating BBQ", "Funny", "www.youtube.com/drew-eating-bbq")
-   expect(video.name).to eq('Drew eating BBQ')
-   expect(video.genre).to eq('Funny')
-   expect(video.url).to eq('www.youtube.com/drew-eating-bbq')
- end
+  it "initializes a video with a name, genre, and url" do
+    video = Video.new("Drew eating BBQ", "Funny", "www.youtube.com/drew-eating-bbq")
+    
+    expect(video.name).to eq('Drew eating BBQ')
+    expect(video.genre).to eq('Funny')
+    expect(video.url).to eq('www.youtube.com/drew-eating-bbq')
+  end
 end
